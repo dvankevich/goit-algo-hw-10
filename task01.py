aae7a05fe8f371767672aaec74e3409a1cbd345c
dvankevich <dvankevich@gmail.com>
2025-06-16 21:00:16 +0300

    fix limitations

--- task01.py
+++ task01.py
@@ -1,14 +1,14 @@
 import pulp
 
 # Ініціалізація моделі
-model = pulp.LpProblem("Maximize Profit", pulp.LpMaximize)
+model = pulp.LpProblem("Maximize_Products", pulp.LpMaximize)
 
 # Визначення змінних
 lemonade = pulp.LpVariable("Лимонад", lowBound=0, cat='Integer')
-fruit_juice = pulp.LpVariable("Фруктовий сік", lowBound=0, cat='Integer')
+fruit_juice = pulp.LpVariable("Фруктовий_сік", lowBound=0, cat='Integer')
 
 # Цільова функція: максимізувати загальну кількість вироблених продуктів "Лимонад" та "Фруктовий сік"
-model += lemonade + fruit_juice, "Загальна кількість продуктів"
+model += lemonade + fruit_juice, "Загальна_кількість_продуктів"
 
 # Обмеження на ресурси
 '''
@@ -17,17 +17,21 @@ model += lemonade + fruit_juice, "Загальна кількість проду
 3. Обмеження ресурсів: 100 од. "Води", 50 од. "Цукру", 30 од. "Лимонного соку" та 40 од. "Фруктового пюре".
 4. Виробництво одиниці "Лимонаду" вимагає 2 од. "Води", 1 од. "Цукру" та 1 од. "Лимонного соку".
 5. Виробництво одиниці "Фруктового соку" вимагає 2 од. "Фруктового пюре" та 1 од. "Води".
+            Вода    Цукор   Л.сік   Пюре
+лимонад       2       1       1
+сік           1                       1
+обмеження   <=100    <=50    <=30   <=40
 '''
-model += 2 * lemonade + 1 * fruit_juice <= 100, "Вода"
-model += 1 * lemonade <= 50, "Цукор"
-model += 1 * lemonade <= 30, "Лимонний сік"
-model += 2 * fruit_juice + 1 * lemonade <= 40, "Фруктове пюре"
+model += 2 * lemonade + fruit_juice <= 100, "Вода"
+model += lemonade <= 50, "Цукор"
+model += lemonade <= 30, "Лимонний_сік"
+model += 2 * fruit_juice <= 40, "Фруктове_пюре"
 
 # Розв'язання моделі
 model.solve()
 
 # Вивід результатів
 print(f"Status: {pulp.LpStatus[model.status]}")
-print(f"Виробляти лимонадів: {lemonade.varValue}")
+print(f"Виробляти лимонаду: {lemonade.varValue}")
 print(f"Виробляти фруктового соку: {fruit_juice.varValue}")
 print(f"Загальна кількість продуктів: {lemonade.varValue + fruit_juice.varValue}")
