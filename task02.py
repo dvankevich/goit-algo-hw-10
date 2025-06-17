4f9435822211592646330747c5a423fa07a00ff6
dvankevich <dvankevich@gmail.com>
2025-06-17 20:05:01 +0300

    conclusions

--- task02.py
+++ task02.py
@@ -3,43 +3,40 @@ import numpy as np
 import scipy.integrate as spi
 import random
 
-# Визначення функції та межі інтегрування
+# Визначення функції
 def f(x):
-    return x ** 2
+    #return x ** 2
+    return np.sin(x) + np.cos(x) + 2
 
-def is_inside(func, x, y):
+def is_inside(x, y, func):
     """Перевіряє, чи знаходиться точка (x, y) всередині "сірої зони"."""
     return y <= func(x)
 
 def monte_carlo_simulation(a, b, num_experiments, func):
     """Виконує серію експериментів методом Монте-Карло."""
     average_area = 0
+    maxf = max(func(x) for x in np.linspace(a, b, 100))
+    #print("maxf", maxf)
 
     for _ in range(num_experiments):
-        # Генерація випадкових точок
-        points = [(random.uniform(0, a), random.uniform(0, b)) for _ in range(15000)]
-        # Відбір точок, що знаходяться всередині трикутника
+        points = [(random.uniform(a, b), random.uniform(0, maxf)) for _ in range(15000)]
         inside_points = [point for point in points if is_inside(point[0], point[1], func)]
 
-        # Розрахунок площі за методом Монте-Карло
         M = len(inside_points)
         N = len(points)
-        area = (M / N) * (a * b)
-
-        # Додавання до середньої площі
+        area = (M / N) * ((b - a) * maxf)
         average_area += area
 
-    # Обчислення середньої площі
     average_area /= num_experiments
     return average_area
 
 a = 0.5  # Нижня межа
-b = 2  # Верхня межа
+b = 6.25  # Верхня межа
 # Кількість експериментів
-num_experiments = 1
+num_experiments = 100
 
 # Створення діапазону значень для x
-x = np.linspace(-0.5, 2.5, 400)
+x = np.linspace(-0.5, 7.5, 400)
 y = f(x)
 
 # Створення графіка
@@ -62,9 +59,10 @@ ax.set_ylabel('f(x)')
 # Додавання меж інтегрування та назви графіка
 ax.axvline(x=a, color='gray', linestyle='--')
 ax.axvline(x=b, color='gray', linestyle='--')
-ax.set_title('Графік інтегрування f(x) = x^2 від ' + str(a) + ' до ' + str(b))
+ax.set_title('Графік інтегрування f(x) від ' + str(a) + ' до ' + str(b))
 plt.grid()
-#plt.show()
+print("Для продовження закрийте графік")
+plt.show()
 
 result, error = spi.quad(f, a, b)
 
